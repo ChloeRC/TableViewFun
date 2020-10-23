84e1006fa86070030021ffa036504d027c48ce35
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-10-23 11:36:31 -0700

    end of class

--- TableViewFunS2/DogTableViewController.swift
+++ TableViewFunS2/DogTableViewController.swift
@@ -17,7 +17,7 @@ import UIKit
 // we will use #1 because ADS uses #2
 
 
-class DogTableViewController: UIViewController {
+class DogTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var dogs = [Dog]()
     
@@ -35,5 +35,34 @@ class DogTableViewController: UIViewController {
         dogs.append(Dog(name: "Spike", breed: "Bulldog"))
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // table view asks data source:
+        // how many rows should I put in this section?
+        
+        // we are only going to have one section
+        if (section == 0) {
+            return dogs.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // table view asks data source:
+        // what cell should I put at this indexPath?
+        
+        // indexPath has 2 properties
+        // section number (ignore for now)
+        // row number (corresponds to array index number)
+        let row = indexPath.row
+        let dog = dogs[row]
+        // we want a cell that displays this dog's name and breed
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DogCell", for: indexPath) as! DogTableViewCell
+        // why a reusable cell?
+        // what if dogs had a big amount of dogs in it
+        // we wouldn't need that many cells, becasue they all wouldn't be on the screen at the same time
+        
+        cell.update(with: dog)
+        return cell
+    }
 }
 
